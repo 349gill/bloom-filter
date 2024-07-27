5490924f42e73732433d17aa37ac3415e2cd0839
Harsh Gill <112195414+349gill@users.noreply.github.com>
2024-07-26 20:56:09 -0600

    Update BloomFilterTest.java

--- BloomFilterTest.java
+++ BloomFilterTest.java
@@ -4,10 +4,12 @@ public class BloomFilterTest {
     public static void main(String[] args) {
         BloomFilter b = new BloomFilter();
 
+        System.out.print("False Positivity before the first element: ");
+        System.out.println(b.false_positivity());
         for (int i = 0; i < 4800; i++) {
             b.add((i));
-            System.out.print("False Positivity: ");
-            System.out.println(b.false_positivity());
         }
+        System.out.print("False Positivity after the 4800th element: ");
+        System.out.println(b.false_positivity());
     }
 }
